782bd034300f882b8868b3c4a29b00d015b509f7
Victoria Arriola <arriova16@gmail.com>
2024-07-29 14:19:41 -0500

    changing file name
    Just saving into location rather than going through all raw folder

--- Formatting/TBH_formatter.m
+++ Formatting/TBH_formatter.m
@@ -2,28 +2,49 @@
 %formatting and saving within folder
 
 tld = 'Z:\UserFolders\ToriArriola\DARPA_updated\RawData';
+process_loc = 'Z:\UserFolders\ToriArriola\DARPA_updated\PreProcessedData\Whistlepig\DarpaBlock_Hybrid';
 monkey_list = dir(tld);
 monkey_list = monkey_list(3:end);
 
 %% Formatting files in folder
 
-data = struct(); ii = 1;
+
+
+
+
+
+
+
+
+
+%%
+
+% data = struct(); ii = 1;
 for m = 1:length(monkey_list) %monkey names
     %get list of electrodes
     electrode_list = dir(fullfile(tld, monkey_list(m).name, 'Electrode*'));
-    for e = 1:size(electrode_list,1)
+    for e = 1:size(electrode_list,1) %for each electrode
         electrode_split = strsplit(electrode_list(e).name, '_');
         electrode_number = electrode_split{2};
         if contains(electrode_number, 'and')
             and_idx = strfind(electrode_number, 'and');
             ee = [str2double(electrode_number(1:and_idx-1)), str2double(electrode_number(and_idx+3:end))];
         end
+
+        threshold_f = fullfile(tld, monkey_list(m).name, electrode_list(e).name, 'ThresholdTask');
+        % subf_threshold = dir(fullfile(threshold_f))
         
-        block_subf = fullfile(tld, monkey_list(m).name, electrode_list(e).name, 'BlockTask');
-        block_subf_subf = dir(fullfile(block_subf)); 
+        % block_f = fullfile(tld, monkey_list(m).name, electrode_list(e).name, 'BlockTask');
+        % subf_block = dir(fullfile(block_f, 'Amp*'));
+        % 
+        %  block_file_list = dir(fullfile(subf_block, '.rsp'));
+        
+        % 
+        % % % block_subf = fullfile(tld, monkey_list(m).name, electrode_list(e).name, 'BlockTask');
+        % block_subf_subf = dir(fullfile(block_subf)); 
         
          % fullfile(block_subf_subf(3).name)
-         
+
         % threshold_file_list = dir(fullfile(subf, '*.rsp'));
  % 
         % block_tld = fullfile(tld, monkey_list(m).name, electrode_list(e).name, 'BlockTask');
