374c6ddc7c21f168950dc84a8e4b84d0d4c68f2d
Victoria Arriola <arriova16@gmail.com>
2024-08-14 16:10:53 -0500

    ugh

--- Analysis/cathodic_anodic_analysis.m
+++ Analysis/cathodic_anodic_analysis.m
@@ -15,10 +15,12 @@ monkey_list = dir(tld);
 for m = 3:size(monkey_list)
      for t = 1:length(task_type)
      subf_og = fullfile(tld, monkey_list(m).name, 'DarpaOG');
-     subf_ca_an = fullfile(tld, monkey_list(3).name, 'Cathodic_Anodic');
+     dir_str = fullfile(subf_og, ['*', task_type{t}, 'Task*combtable.mat']);
+     flist = dir(dir_str);
+     % subf_ca_an = fullfile(tld, monkey_list(3).name, 'Cathodic_Anodic');
      
      og_mat_files = dir(fullfile(subf_og, '*mat'));
-     ca_an_mat_files = dir(fullfile(subf_ca_an, '*mat'));
+     % ca_an_mat_files = dir(fullfile(subf_ca_an, '*mat'));
     
          for g = 1:size(og_mat_files, 1)
             fname_split = strsplit(og_mat_files(g).name, '_');
@@ -29,6 +31,7 @@ for m = 3:size(monkey_list)
             end
             
             og_struct(ii).Monkey = monkey_list(m).name;
+            %incorrect = not loading files in based on correct task
             og_struct(ii).Task = task_type{t};
             og_struct(ii).Electrodes = sort(ee);
             
